0a872ad04491218bb2ef63ea0a8f13eb94aea8e5
fabian emiliano rayon sotelo <emiliano8907@gmail.com>
2024-03-01 14:08:21 -0600

    intro php

---
+++ ciclos.php
@@ -0,0 +1,19 @@
+<?php
+/*include ('functions.php');
+include_once('functions.php');
+require('functions.php');
+require_once('functions.php');*/
+
+echo "ciclo for:\n";
+for ($i=0; $i < 10 ; $i++) { 
+    echo "valore de i: $i";
+}
+
+echo "ciclo while" . PHP_EOL;
+$a=0;
+while ($a <= 10) {
+    echo "valor de a:\n";
+    $a++;
+    
+}
+?>
